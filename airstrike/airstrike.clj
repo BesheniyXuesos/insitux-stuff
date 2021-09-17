00ae757ea2a3b6a25cee70e96905868962115387
CauseLovePivo <svyatislav.cvc@gmail.com>
2021-09-17 22:43:57 +0300

    Update airstrike.clj

--- airstrike/airstrike.clj
+++ airstrike/airstrike.clj
@@ -1,18 +1,18 @@
-(var pos $dl.players.PLAYERNAME.position) 
-;change PLAYERNAME to your nickname to get your coordinates
-;you can write coordinates instead of player position for example: (var pos [1 1 1])
+	(var pos $dl.players.svyat2.position) 
+	;change PLAYERNAME to your nickname to get your coordinates
+	;you can write coordinates instead of player position for example: (var pos [1 1 1])
 
-(var randomMax 20)
-(var randomMin (* randomMax -1))
-;you can also change these variables to get more or less spread
+	(var randomMax 20)
+	(var randomMin (* randomMax -1))
+	;you can also change these variables to get more or less spread
 
-(let g 0)
-(dl.sound.play "rbxassetid://6959070005")
-(var a (rand-int -3 3))
-(var b (rand-int -3 3))
-(wait 15)
- (while (< g 30)
-    (print g)
-    (let g (inc g))
-    (dl.util.explosion (map + pos [(+(rand-int randomMin randomMax) (* g a)) 0 (+ (rand-int randomMin randomMax) (* g b)) ]) "TestGrenade")
-(wait  (/(rand-int 1 10)100)))
+	(let g 0)
+	(dl.sound.play "rbxassetid://6959070005")
+	(var a (rand-int -3 3))
+	(var b (rand-int -3 3)
+	(wait 15)
+	 (while (< g (rand-int 20 35))
+	    (print g)
+	    (let g (inc g))
+	    (dl.util.explosion (map + pos [(+(rand-int randomMin randomMax) (* g a)) 0 (+ (rand-int randomMin randomMax) (* g b)) ]) "TestGrenade")
+	(wait  (/(rand-int 1 10)100)))
