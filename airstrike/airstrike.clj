a97f4428a6b6319d8f4322951c18be509b628331
CauseLovePivo <svyatislav.cvc@gmail.com>
2021-09-18 17:40:29 +0300

    Update airstrike.clj

--- airstrike/airstrike.clj
+++ airstrike/airstrike.clj
@@ -1,18 +1,54 @@
-	(var pos $dl.players.PLAYERNAME.position) 
-	;change PLAYERNAME to your nickname to get your coordinates
-	;you can write coordinates instead of player position for example: (var pos [1 1 1])
+(var randomMax 20)
+(var randomMin (* randomMax -1))
+;you can also change these variables to get more or less spread
 
-	(var randomMax 20)
-	(var randomMin (* randomMax -1))
-	;you can also change these variables to get more or less spread
 
-	(let g 0)
-	(dl.sound.play "rbxassetid://6959070005")
-	(var a (rand-int -3 3))
-	(var b (rand-int -3 3)
-	(wait 15)
-	 (while (< g (rand-int 20 35))
-	    (print g)
-	    (let g (inc g))
-	    (dl.util.explosion (map + pos [(+(rand-int randomMin randomMax) (* g a)) 0 (+ (rand-int randomMin randomMax) (* g b)) ]) "TestGrenade")
-	(wait  (/(rand-int 1 10)100)))
+(function airtstikePlr sender		
+		(var pos (position (str "dl.players." sender)))  
+						;change PLAYERNAME to your nickname to get your coordinates
+						;you can write coordinates instead of player position for example: (var pos [1 1 1]
+
+						(let g 0)
+						(dl.sound.play "rbxassetid://6959070005")
+
+						(var a (rand-int -3 3))
+						(var b (rand-int -3 3))
+
+						(wait 14.5)
+						 (while (< g (rand-int 20 40))
+						    (print g)
+						    (let g (inc g))
+						    (dl.util.explosion (map + pos [(+(rand-int randomMin randomMax) (* g a)) 0 (+ (rand-int randomMin randomMax) (* g b)) ]) "TestGrenade")
+						(wait  (/(rand-int 3 15)100))) 
+)
+
+(function airtstikePos sender		
+		(var pos sender)  
+						;change PLAYERNAME to your nickname to get your coordinates
+						;you can write coordinates instead of player position for example: (var pos [1 1 1]
+
+						(let g 0)
+						(dl.sound.play "rbxassetid://6959070005")
+
+						(var a (rand-int -3 3))
+						(var b (rand-int -3 3))
+
+						(wait 14.5)
+						 (while (< g (rand-int 20 40))
+						    (print g)
+						    (let g (inc g))
+						    (dl.util.explosion (map + pos [(+(rand-int randomMin randomMax) (* g a)) 0 (+ (rand-int randomMin randomMax) (* g b)) ]) "TestGrenade")
+						(wait  (/(rand-int 3 15)100))) 
+)
+
+(function sent sender channel message
+  	(if (starts-with? message "!airstrikePlr ")
+					(airtstikePlr (sect message 14))
+			)
+  	(if (starts-with? message "!airstrikePos ")
+					(airtstikePos (sect message 14))
+			)
+ )
+
+(dl.events.on_chat_message.connect sent)
+
